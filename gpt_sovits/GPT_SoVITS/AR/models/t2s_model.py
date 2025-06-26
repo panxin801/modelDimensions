e35655cbefb0095341887144d4239efc79c9dd71
panxin801 <panxinecho@qq.com>
2025-06-26 14:04:10 +0800

    bugfix/t2s_model error

--- gpt_sovits/GPT_SoVITS/AR/models/t2s_model.py
+++ gpt_sovits/GPT_SoVITS/AR/models/t2s_model.py
@@ -402,28 +402,29 @@ class Text2SemanticDecoder(nn.Module):
 
     def forward_old(self, x, x_lens, y, y_lens, bert_feature):
         """
-        x: phoneme_ids: [B, T_phone, 512]
+        x: phoneme_ids: [B, T_phone]
         x_lens: [B]
         y: semantic_ids, from vq_model.extract_latent  [B, T_semantic]
         y_lens: [B]
         bert_feature: bert feature from text # [B, 1024, T_phone]
         """
-        x = self.ar_text_embedding(x)
-        x = x + self.bert_proj(bert_feature.transpose(1, 2))
-        x = self.ar_text_position(x)
+        x = self.ar_text_embedding(x)  # [B,T_phone,512]
+        x = x + self.bert_proj(bert_feature.transpose(1, 2))  # [B,T_phone,512]
+        x = self.ar_text_position(x)  # [B,T_phone,512]
 
         x_mask = make_pad_mask(x_lens)  # [B, T_phone]
         y_mask = make_pad_mask(y_lens)  # [B, T_semantic]
         y_mask_int = y_mask.type(torch.int64)
-        codes = y.type(torch.int64) * (1 - y_mask_int)
+        codes = y.type(torch.int64) * (1 - y_mask_int)  # [B, T_semantic]
 
         # Training
         # AR Decoder
+        # y=targets=[B, T_semantic]
         y, targets = self.pad_y_eos(codes, y_mask_int, eos_id=self.EOS)
         x_len = x_lens.max()
         y_len = y_lens.max()
-        y_emb = self.ar_audio_embedding(y)  # [B,T_audio,512]
-        y_pos = self.ar_audio_position(y_emb)
+        y_emb = self.ar_audio_embedding(y)  # [B,T_semantic,512]
+        y_pos = self.ar_audio_position(y_emb)  # [B,T_semantic,512]
 
         xy_padding_mask = torch.concat(
             [x_mask, y_mask], dim=1)  # [B,T_phone+T_audio]
@@ -442,24 +443,28 @@ class Text2SemanticDecoder(nn.Module):
             (x_len, 0),
             value=False,
         )  # [y_len,x_len+y_len]
+        # [x_len+y_len,x_len+y_len]
         xy_attn_mask = torch.concat([x_attn_mask, y_attn_mask], dim=0)
         bsz, src_len = x.shape[0], x_len + y_len
         _xy_padding_mask = (
             ar_xy_padding_mask.view(bsz, 1, 1, src_len)
             .expand(-1, self.num_head, -1, -1)
             .reshape(bsz * self.num_head, 1, src_len)
-        )
-        xy_attn_mask = xy_attn_mask.logical_or(_xy_padding_mask)
+        )  # [self.num_head,B,src_len]
+        xy_attn_mask = xy_attn_mask.logical_or(
+            _xy_padding_mask)  # [self.num_head,src_len,src_len]
+        # [self.num_head,src_len,src_len]
         new_attn_mask = torch.zeros_like(xy_attn_mask, dtype=x.dtype)
         new_attn_mask.masked_fill_(xy_attn_mask, float("-inf"))
-        xy_attn_mask = new_attn_mask
+        xy_attn_mask = new_attn_mask  # [self.num_head,src_len,src_len]
         # x 和完整的 y 一次性输入模型
         xy_pos = torch.concat([x, y_pos], dim=1)  # [B,T_phone+T_audio,512]
         xy_dec, _ = self.h(
             (xy_pos, None),
             mask=xy_attn_mask,
         )  # [B,T_phone+T_audio,512]
-        logits = self.ar_predict_layer(xy_dec[:, x_len:]).permute(0, 2, 1)
+        logits = self.ar_predict_layer(xy_dec[:, x_len:]).permute(
+            0, 2, 1)  # [B,1025, T_audio]
         # loss
         # from feiteng: 每次 duration 越多, 梯度更新也应该更多, 所以用 sum
         loss = F.cross_entropy(logits, targets, reduction="sum")
