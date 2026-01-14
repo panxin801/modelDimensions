1c082af8b2342b99e82a5de4107ef5410926ae57
panxin801 <panxinecho@qq.com>
2026-01-14 21:36:20 +0800

    0114 add

--- cosyvoice/cosyvoice/transformer/encoder.py
+++ cosyvoice/cosyvoice/transformer/encoder.py
@@ -25,6 +25,10 @@ from cosyvoice.utils.class_utils import (
     COSYVOICE_SUBSAMPLE_CLASSES,
     COSYVOICE_ATTENTION_CLASSES,
     COSYVOICE_ACTIVATION_CLASSES,)
+from cosyvoice.transformer.encoder_layer import (
+    ConformerEncoderLayer, TransformerEncoderLayer)
+from cosyvoice.transformer.positionwise_feed_forward import PositionwiseFeedForward
+from cosyvoice.transformer.convolution import ConvolutionModule
 
 
 class BaseEncoder(nn.Module):
@@ -93,6 +97,160 @@ class BaseEncoder(nn.Module):
             COSYVOICE_EMB_CLASSES[pos_enc_layer_type](output_size, positional_dropout_rate))
         self.after_norm = nn.LayerNorm(output_size, eps=1e-5)
 
+    def output_size(self,) -> int:
+        return self._output_size
 
-class ConformerEncoder:
-    ...
+
+class TransformerEncoder(BaseEncoder):
+    """Transformer encoder module."""
+
+    def __init__(self,
+                 input_size: int,
+                 output_size: int = 256,
+                 attention_heads: int = 4,
+                 linear_units: int = 2048,
+                 num_blocks: int = 6,
+                 dropout_rate: float = 0.1,
+                 positional_dropout_rate: float = 0.1,
+                 attention_dropout_rate: float = 0.0,
+                 input_layer: str = "conv2d",
+                 pos_enc_layer_type: str = "abs_pos",
+                 normalize_before: bool = True,
+                 static_chunk_size: int = 0,
+                 use_dynamic_chunk: bool = False,
+                 global_cmvn: torch.nn.Module = None,
+                 use_dynamic_left_chunk: bool = False,
+                 key_bias: bool = True,
+                 selfattention_layer_type: str = "selfattn",
+                 activation_type: str = "relu",
+                 gradient_checkpointing: bool = False,
+                 ):
+        """ Construct TransformerEncoder
+        See Encoder for the meaning of each parameter.
+        """
+        super().__init__(input_size,
+                         output_size,
+                         attention_heads,
+                         linear_units,
+                         num_blocks,
+                         dropout_rate,
+                         positional_dropout_rate,
+                         attention_dropout_rate,
+                         input_layer,
+                         pos_enc_layer_type,
+                         normalize_before,
+                         static_chunk_size,
+                         use_dynamic_chunk, global_cmvn,
+                         use_dynamic_left_chunk,
+                         gradient_checkpointing)
+        activation = COSYVOICE_ACTIVATION_CLASSES[activation_type]()
+        self.encoders = nn.ModuleList([
+            TransformerEncoderLayer(output_size,
+                                    COSYVOICE_ATTENTION_CLASSES[selfattention_layer_type](attention_heads,
+                                                                                          output_size,
+                                                                                          attention_dropout_rate,
+                                                                                          key_bias),
+                                    PositionwiseFeedForward(output_size,
+                                                            linear_units,
+                                                            dropout_rate,
+                                                            activation),
+                                    dropout_rate, normalize_before) for _ in range(num_blocks)
+        ])
+
+
+class ConformerEncoder(BaseEncoder):
+    """Conformer encoder module."""
+
+    def __init__(self,
+                 input_size: int,
+                 output_size: int = 256,
+                 attention_heads: int = 4,
+                 linear_units: int = 2048,
+                 num_blocks: int = 6,
+                 dropout_rate: float = 0.1,
+                 positional_dropout_rate: float = 0.1,
+                 attention_dropout_rate: float = 0.0,
+                 input_layer: str = "conv2d",
+                 pos_enc_layer_type: str = "rel_pos",
+                 normalize_before: bool = True,
+                 static_chunk_size: int = 0,
+                 use_dynamic_chunk: bool = False,
+                 global_cmvn: torch.nn.Module = None,
+                 use_dynamic_left_chunk: bool = False,
+                 positionwise_conv_kernel_size: int = 1,
+                 macaron_style: bool = True,
+                 selfattention_layer_type: str = "rel_selfattn",
+                 activation_type: str = "swish",
+                 use_cnn_module: bool = True,
+                 cnn_module_kernel: int = 15,
+                 causal: bool = False,
+                 cnn_module_norm: str = "batch_norm",
+                 key_bias: bool = True,
+                 gradient_checkpointing: bool = False,
+                 ):
+        """Construct ConformerEncoder
+        Args:
+            input_size to use_dynamic_chunk, see in BaseEncoder
+            positionwise_conv_kernel_size (int): Kernel size of positionwise
+                conv1d layer.
+            macaron_style (bool): Whether to use macaron style for
+                positionwise layer.
+            selfattention_layer_type (str): Encoder attention layer type,
+                the parameter has no effect now, it's just for configure
+                compatibility.
+            activation_type (str): Encoder activation function type.
+            use_cnn_module (bool): Whether to use convolution module.
+            cnn_module_kernel (int): Kernel size of convolution module.
+            causal (bool): whether to use causal convolution or not.
+            key_bias: whether use bias in attention.linear_k, False for whisper models.
+        """
+
+        super().__init__(input_size,
+                         output_size,
+                         attention_heads,
+                         linear_units,
+                         num_blocks,
+                         dropout_rate,
+                         positional_dropout_rate,
+                         attention_dropout_rate,
+                         input_layer,
+                         pos_enc_layer_type,
+                         normalize_before,
+                         static_chunk_size,
+                         use_dynamic_chunk,
+                         global_cmvn,
+                         use_dynamic_left_chunk,
+                         gradient_checkpointing)
+        activation = COSYVOICE_ACTIVATION_CLASSES[activation_type]()
+
+        # self-attention module args definition
+        encoder_selfattn_layer = (attention_heads,
+                                  output_size,
+                                  attention_dropout_rate,
+                                  key_bias)
+        # feed-forward module args definition
+        positionwise_layer_args = (output_size,
+                                   linear_units,
+                                   dropout_rate,
+                                   activation)
+        # convolution module definition
+        convolution_layer_args = (output_size,
+                                  cnn_module_kernel,
+                                  activation,
+                                  cnn_module_norm,
+                                  causal)
+
+        self.encoders = nn.ModuleList([
+            ConformerEncoderLayer(output_size,
+                                  COSYVOICE_ATTENTION_CLASSES[selfattention_layer_type](
+                                      *encoder_selfattn_layer),
+                                  PositionwiseFeedForward(
+                                      *positionwise_layer_args),
+                                  PositionwiseFeedForward(
+                                      *positionwise_layer_args) if macaron_style else None,
+                                  ConvolutionModule(
+                                      *convolution_layer_args) if use_cnn_module else None,
+                                  dropout_rate,
+                                  normalize_before,
+                                  ) for _ in range(num_blocks)
+        ])
---
+++ cosyvoice/cosyvoice/transformer/positionwise_feed_forward.py
@@ -0,0 +1,112 @@
+# Copyright (c) 2019 Shigeki Karita
+#               2020 Mobvoi Inc (Binbin Zhang)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Positionwise feed forward layer definition."""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PositionwiseFeedForward(nn.Module):
+    """Positionwise feed forward layer.
+    FeedForward are appied on each position of the sequence.
+    The output dim is same with the input dim.
+
+    Args:
+        idim (int): Input dimenstion.
+        hidden_units (int): The number of hidden units.
+        dropout_rate (float): Dropout rate.
+        activation (torch.nn.Module): Activation function
+    """
+
+    def __init__(self,
+                 idim: int,
+                 hidden_unit: int,
+                 dropout_rate: float,
+                 activation: nn.Module = nn.ReLU()):
+        """Construct a PositionwiseFeedForward object."""
+        super().__init__()
+
+        self.w_1 = nn.Linear(idim, hidden_unit)
+        self.activatation = activation
+        self.dropout = nn.Dropout(dropout_rate)
+        self.w_2 = nn.Linear(hidden_unit, idim)
+
+    def forward(self, xs: torch.Tensor) -> torch.Tensor:
+        """Forward function.
+        Args:
+            xs: input tensor (B, L, D)
+        Returns:
+            output tensor, (B, L, D)
+        """
+        return self.w_2(self.dropout(self.activatation(self.w_1(xs))))
+
+
+class MoEFFNLayer(torch.nn.Module):
+    """
+    Mixture of expert with Positionwise feed forward layer
+    See also figure 1 in https://arxiv.org/pdf/2305.15663.pdf
+    The output dim is same with the input dim.
+
+    Modified from https://github.com/Lightning-AI/lit-gpt/pull/823
+                  https://github.com/mistralai/mistral-src/blob/b46d6/moe_one_file_ref.py#L203-L219
+    Args:
+        n_expert: number of expert.
+        n_expert_per_token: The actual number of experts used for each frame
+        idim (int): Input dimenstion.
+        hidden_units (int): The number of hidden units.
+        dropout_rate (float): Dropout rate.
+        activation (torch.nn.Module): Activation function
+    """
+
+    def __init__(self,
+                 n_experts: int,
+                 n_expert_per_token: int,
+                 idim: int,
+                 hidden_units: int,
+                 dropout_rate: float,
+                 activation: nn.Module = nn.ReLU()):
+        super().__init__()
+
+        self.gate = nn.Linear(idim, n_experts, bias=False)
+        self.experts = nn.ModuleList(
+            [PositionwiseFeedForward(
+                idim, hidden_units, dropout_rate, activation) for _ in range(n_experts)]
+        )
+        self.n_expert_per_token = n_expert_per_token
+
+    def forward(self, xs: torch.Tensor) -> torch.Tensor:
+        """Foward function.
+        Args:
+            xs: input tensor (B, L, D)
+        Returns:
+            output tensor, (B, L, D)
+        """
+
+        B, L, D = xs.size()  # batch size, sequence length, embedding dimension (idim)
+        xs = xs.view(-1, D)  # (B*L,D)
+        router = self.gate(xs)  # (B*L, n_experts)
+        # probs:(B*L, n_expert), indices: (B*L, n_expert)
+        logits, indices = torch.topk(router, self.n_expert_per_token)
+        weights = F.softmax(logits, dim=1, dtype=torch.float).to(
+            dtype=xs.dtype)  # (B*L, n_expert_per_token)
+        output = torch.zeros_like(xs)  # (B*L, D)
+
+        for i, expert in enumerate(self.experts):
+            mask = indices == i
+            batch_idx, ith_expert = torch.where(mask)
+            output[batch_idx] += weights[batch_idx,
+                                         ith_expert, None] * expert(xs[batch_idx])
+        return output.view(B, L, D)
