66f5e810921b9c9b986d5a6b7fb4ecbbbab763e7
panxin801 <panxinecho@qq.com>
2026-01-26 21:21:33 +0800

    change path

--- cosyvoice/example.py
+++ cosyvoice/example.py
@@ -20,16 +20,16 @@ def cosyvoice_example():
     cosyvoice = AutoModel(
         model_dir="/data/cosyvoicePretrainModels/pretrained_models/CosyVoice-300M")
     # zero_shot usage
-    for i, j in enumerate(cosyvoice.inference_zero_shot("收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。", "希望你以后能够做的比我还好呦。", "asset\zero_shot_prompt.wav")):
+    for i, j in enumerate(cosyvoice.inference_zero_shot("收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。", "希望你以后能够做的比我还好呦。", "asset/zero_shot_prompt.wav")):
         torchaudio.save(f"zero_shot_{i}.wav",
                         j["tts_speech"], cosyvoice.sample_rate)
     # cross_lingual usage, <|zh|><|en|><|ja|><|yue|><|ko|> for Chinese/English/Japanese/Cantonese/Korean
     for i, j in enumerate(cosyvoice.inference_cross_lingual("< |en | >And then later on, fully acquiring that company. So keeping management in line, interest in line with the asset that\'s coming into the family is a reason why sometimes we don\'t buy the whole thing.",
-                                                            "./asset/cross_lingual_prompt.wav")):
+                                                            "asset/cross_lingual_prompt.wav")):
         torchaudio.save(f"cross_lingual_{i}.wav",
                         j["tts_speech"], cosyvoice.sample_rate)
     # vc usage
-    for i, j in enumerate(cosyvoice.inference_vc("./asset/cross_lingual_prompt.wav", "./asset/zero_shot_prompt.wav")):
+    for i, j in enumerate(cosyvoice.inference_vc("asset/cross_lingual_prompt.wav", "asset/zero_shot_prompt.wav")):
         torchaudio.save(f"vc_{i}.wav", j["tts_speech"], cosyvoice.sample_rate)
 
     cosyvoice = AutoModel(
